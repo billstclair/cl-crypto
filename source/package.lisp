29969d155cafccf5e630ef13902f0046e3da23d2
mrbug <mrbug@rayservers.net>
2010-08-14 14:31:20 -0400

    Initial commit.

---
+++ source/package.lisp
@@ -0,0 +1,13 @@
+(defpackage crypto
+  (:use common-lisp)
+  (:export
+   ;; AES interface
+   #:aes-expand-key
+   #:aes-encrypt
+   #:aes-decrypt
+   ;; Tests
+   #:aes-self-test
+   #:aes-get-speed
+   #:aes-get-avg-speed))
+
+  
